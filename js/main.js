d0010f751a8ae6ef7f1c0dea22070fdab3882de9
Idan Dujovne <idan.duj@gmail.com>
2023-11-15 21:39:39 +0200

    Change add ball size with getRandomInt

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 'use strict'
 
 function onBallClick() {
-    var elBall = document.querySelector('.ball')
-    var size = +elBall.innerText + 50
+    const elBall = document.querySelector('.ball')
+    const increment = getRandomInt(20, 61)
+    var size = +elBall.innerText + increment
+
     if (size > 400) size = 100
     elBall.style.height = size + 'px'
     elBall.style.width = size + 'px'
-    elBall.innerText =  size
+    elBall.innerText = size
 }
