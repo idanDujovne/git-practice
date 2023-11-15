abbfbd438bcd0967b0202987052700af2c3d74d4
Idan Dujovne <idan.duj@gmail.com>
2023-11-15 20:55:17 +0200

    Limit ball max size

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@
 function onBallClick() {
     var elBall = document.querySelector('.ball')
     var size = +elBall.innerText + 50
+    if (size > 400) size = 100
     elBall.style.height = size + 'px'
     elBall.style.width = size + 'px'
     elBall.innerText =  size
